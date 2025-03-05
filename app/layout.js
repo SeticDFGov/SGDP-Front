6e77fc1290f59e7e3ad25a1bcd0a694c699012da
DeM4rcio <henriquemarcio170@gmail.com>
2025-03-05 15:00:56 -0300

    remove sessions

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import  SessionProvider  from "./SessionProvider";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@
 
 import React, { useEffect, useState, useRef } from "react";
 import Dashboard from "./components/Dasjboard";
-import { SessionProvider } from "next-auth/react";
+
 
 const Home = () => {
   
