f3145b8613b00072a8d338584b39daa8b1ecd12e
DeM4rcio <henriquemarcio170@gmail.com>
2025-03-05 15:16:59 -0300

    alter url

--- app/services/authService.js
+++ app/services/authService.js
@@ -1,4 +1,4 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000";
+const API_URL = process.env.NEXT_API_URL_AUTH || "http://localhost:5000";
 
 
 export const login = async () => {
@@ -6,11 +6,11 @@ export const login = async () => {
  
 
     try {
-    window.location.href = "http://localhost:5000/api/auth/login";
+    window.location.href = `${API_URL}`;
     if (!response.ok) throw new Error("Erro na autenticação");
     localStorage.setItem("authenticated", "true"); // Guarda o estado de login
 
-    return await response.json();
+    return await response.json(); 
   } catch (error) {
     console.error(error);
     return [];
